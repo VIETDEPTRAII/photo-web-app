7e53327c1667e3e119827b656abe563dad484bad
NguyenThanhViet <nguyenthanhviet31031998@gmail.com>
2021-07-18 20:04:34 +0700

    Config sending email on production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -58,5 +60,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
