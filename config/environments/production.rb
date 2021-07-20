e8b7a5af2a247be93d0153db72c9aff8be248639
NguyenThanhViet <nguyenthanhviet31031998@gmail.com>
2021-07-20 21:35:40 +0700

    config mailer and url when sending mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://my-photo-web-app.herokuapp.com', protocal: 'https' }
+  config.action_mailer.default_url_options = { host: 'https://album-web.herokuapp.com', protocal: 'https' }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
